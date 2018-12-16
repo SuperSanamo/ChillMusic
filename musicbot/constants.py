384e629f4780bced6766e46f3fe92b4468724a21
SuperSanamo <45719494+SuperSanamo@users.noreply.github.com>
2018-12-15 19:03:01 -0600

    Add files via upload

---
+++ musicbot/constants.py
@@ -0,0 +1,8 @@
+import os.path
+
+MAIN_VERSION = 'Verson 1.0.0'
+SUB_VERSION = ''
+VERSION = MAIN_VERSION + SUB_VERSION
+
+AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
+DISCORD_MSG_CHAR_LIMIT = 2000
